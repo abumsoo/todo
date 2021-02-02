bdc8f52c6925a4411b094b14d64d4524585cae04
Bum Kim <bumcrystlbum@gmail.com>
2021-02-01 21:00:51 -0500

    initial boilerplate with webpack

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{let e=(()=>{let e=null;return{addNode:l=>{let d={next:null};d.next=e,e=d},head:e}})();e.addNode("hello"),console.log(e)})();
---
+++ src/index.js
@@ -0,0 +1,41 @@
+/* Code organization
+    What needs to happen
+    - Tasks are objects with the following:
+        - title
+        - description
+        - checkbox
+        - deadline
+        - scheduled date
+        - subtasks (future)
+    - Lists are a list of tasks
+    - There will be a default list
+    User can:
+        - create, update, and delete tasks
+        - can sort tasks by deadline, scheduled date, or created date, or user's order
+        - Make a list
+        - Make todo items in any list
+        - reorder tasks
+        - reorder lists
+    Loading screen:
+        - Inbox (main list)
+        - Render list function, specifically render inbox
+*/
+
+class TaskNode {
+    constructor(title, description) {
+        this.title = title;
+        this.description = description;
+        this.next = null;
+    }
+}
+
+class TaskList {
+    constructor(head = null) {
+        this.head = head;
+    }
+}
+
+let t1 = new TaskNode("Make a checklist", "detailed checklist");
+let t2 = new TaskNode("Complete checklist", "check off checklist");
+t1.next = t2;
+let tasks = new TaskList(t1);
